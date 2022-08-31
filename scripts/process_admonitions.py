0fe3a997bd46334377eed5d0d5ab20d6d9e38346
Eric Passmore <eric.passmore@gmail.com>
2022-08-31 10:24:26 -0700

    bug fix: handle multi-line admonitions

--- scripts/process_admonitions.py
+++ scripts/process_admonitions.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
 
 # goes through markdown files updating info, warning, caution, danger, note, and tip flags
-# FROM: [[info | some text here ]]
+# FROM: [[ info ]]
 # TO: :::info
+# :::
+# FROM: [[ info | title ]]
+# TO: :::info title
+# :::
+# FROM: [[ info ]]
+# | some text here\n
+# TO: :::info
+# some text here
+# :::
+# FROM: [[ info | title ]]
+# | some text here\n
+# TO: :::info title
 # some text here
 # :::
 
@@ -26,41 +38,108 @@ file_name_filter = re.compile(".+\.md$")
 
 
 ########### BEHAVIORS ##################
-logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 ##### Class for document
 class Document:
-    has_admonition = False
+    # any match in file used for decision to overwrite file contents
+    has_admonitions = False
+    # different patterns to match , different groups
+    # match [[ type | some suff here ]]
     admonition_pattern = re.compile(r"\[\[([a-z]+)\s*\|\s*([^\]]+)\]\]")
+    # match [[ type ]]
     basic_admonition_pattern = re.compile(r"\[\[([a-z]+)\]\]")
+    ## match | content text for admonition
+    admonition_text_pattern = re.compile(r"\s*\|\s*([^\r\n]+)")
+    # working needed because this is multi-line pattern
+    working_on_admonition = False
+    admonition_type = None
+    admonition_title = None
+    admonition_text = None
+    # entire file contents
     contents = None
 
-    def __init__(self, contents):
-        self.contents = contents
+    # State Vanila: not working on admonition -> print line
+    #      This is initial state
+    #      enter: working_on_admonition is false
+    #      transition: sets working_on_admonition true
+    #      previous state: None
+    # State Working Line1: process line 1 and supress
+    #      enter: working_on_admonition is true
+    #      transition: sets state_change false
+    #      previous state: Vanila
+    # State Working Line2: process line 2 and print
+    #      enter: working_on_admonition is true
+    #             and state_change is false
+    #      transition: reset to init
+    #      previous state: Working Line 1
+    def add_line(self,line):
+        previous_state = self.working_on_admonition
+        # check for and process admonition line1
+        self.find_admonition(line)
+        current_state = self.working_on_admonition
+
+        state_change = previous_state != current_state
+
+        # not working_on_ad.. state is Vanila
+        if not self.working_on_admonition:
+            self.contents = (self.contents if (self.contents is not None) else "") + line
+        else:
+            # working_on_ad.. and no change state is Working Line 2
+            if not state_change:
+                has_text_body = self.process_admonition_text(line)
+                # found text match line print_newblock
+                self.contents = (self.contents if (self.contents is not None) else "") + self.print_newblock()
+                # corner case following text line is not part of admonition so add it back
+                if not has_text_body:
+                    self.contents = (self.contents if (self.contents is not None) else "") + line
+                self.reset_to_init()
+            # working_on_ad and change state is Working Line 1
 
     def needs_update(self):
-        return self.has_admonition and self.contents
+        return self.has_admonitions and self.contents
 
-    def newblock(self, type, value):
-        return f':::{type}\n{value}\n:::'
-
-    def update_metablock(self):
-        match = re.search(self.admonition_pattern,self.contents)
-        if match:
-            self.has_admonition = True
-            logging.debug('\t\t MATCH |'+match.group(1)+'|'+match.group(2)+'|')
-            meta_type = match.group(1)
-            meta_value = match.group(2)
-            newblock = self.newblock(meta_type,meta_value)
-            self.contents = re.sub(self.admonition_pattern, newblock, self.contents)
+    def print_newblock(self):
+        if self.admonition_text is not None:
+            if self.admonition_title is not None:
+                return f':::{self.admonition_type} {self.admonition_title}\n\n{self.admonition_text}\n\n:::\n'
+            else:
+                return f':::{self.admonition_type}\n\n{self.admonition_text}\n\n:::\n'
+        elif self.admonition_title is not None:
+            return f':::{self.admonition_type} {self.admonition_title}\n\n:::\n'
         else:
-            basic = re.search(self.basic_admonition_pattern,self.contents)
+            return f':::{self.admonition_type}\n\n:::\n'
+
+    def reset_to_init(self):
+        # reset data
+        self.working_on_admonition = False
+        self.admonition_type = None
+        self.admonition_title = None
+        self.admonition_text = None
+
+    def process_admonition_text(self, line):
+        match = re.search(self.admonition_text_pattern,line)
+        if match:
+            self.admonition_text = match.group(1)
+        # need to know true/false on match
+        return match
+
+
+    def find_admonition(self, line):
+        match = re.search(self.admonition_pattern,line)
+        if match:
+            self.has_admonitions = True
+            self.working_on_admonition = True
+            logging.debug('\t\t MATCH |'+match.group(1)+'|'+match.group(2)+'|')
+            self.admonition_type = match.group(1)
+            self.admonition_title = match.group(2)
+        else:
+            basic = re.search(self.basic_admonition_pattern,line)
             if basic:
-                self.has_admonition = True
+                self.has_admonitions = True
+                self.working_on_admonition = True
                 logging.debug('\t\t MATCH |'+basic.group(1)+'|')
-                meta_type = basic.group(1)
-                newblock = self.newblock(meta_type,"")
-                self.contents = re.sub(self.basic_admonition_pattern, newblock, self.contents)
+                self.admonition_type = basic.group(1)
 
 ##### END Class Document #######
 
@@ -78,10 +157,12 @@ if not pass_filter:
 # read file get data create document object
 input_file = open(full_file_path, 'r', encoding="utf-8")
 logging.debug(" fullpath = "+full_file_path)
-all_content = input_file.read()
-f_document = Document(all_content)
-f_document.update_metablock()
+all_content = input_file.readlines()
 input_file.close()
+
+f_document = Document()
+for line in all_content:
+    f_document.add_line(line)
 
 # update and write back file
 if f_document.needs_update():
---
+++ testing/markdown/admonitions.backup.md
@@ -0,0 +1,75 @@
+Additional EOSIO Resources:
+* [EOSIO Utilities](10_utilities/index.md) - Utilities that complement the EOSIO software.  
+
+[//]: # (THIS IS A COMMENT REMOVING BROKEN LINKS)  
+[//]: # (Upgrade-Guide-20_upgrade-guide/index.md-EOSIO-version/protocol-upgrade-guide.)  
+[//]: # (Release Notes 30_release-notes/index.md  - All release notes for this EOSIO version.)  
+
+:::info What's Next?
+
+[Install the EOSIO Software](00_install/index.md) before exploring the sections above.
+
+:::
+
+## Description
+
+The `wallet_api_plugin` exposes functionality from the [`wallet_plugin`](../wallet_plugin/index.md) to the RPC API interface managed by the [`http_plugin`](../../../01_nodeos/03_plugins/http_plugin/index.md).
+
+:::caution Caution
+
+This plugin exposes wallets. Therefore, running this plugin on a publicly accessible node is not recommended. As of 1.2.0, the `wallet_api_plugin` is only available through `keosd`. It is no longer supported by `nodeos`.
+
+:::
+
+## Usage
+
+:::info Default address:port
+
+If no optional arguments are used (i.e. `--url` or `--wallet-url`), `cleos` attempts to connect to a local `nodeos` or `keosd` running at localhost `127.0.0.1` and default port `8888`.
+
+:::
+
+* You can also create a JSON snippet that uses clear text JSON for `data` field.
+
+:::info
+
+Be aware that if a clear text `data` field is used, cleos need to fetch copies of required ABIs using `nodeos` API. That operation has a performance overhead on `nodeos`
+
+:::
+
+```JSON
+{
+  "expiration": "2019-08-01T07:15:49",
+  "ref_block_num": 34881,
+  "ref_block_prefix": 2972818865,
+  "max_net_usage_words": 0,
+  "max_cpu_usage_ms": 0,
+  "delay_sec": 0,
+  "context_free_actions": [],
+  "actions": [{
+      "account": "eosio.token",
+      "name": "transfer",
+      "authorization": [{
+          "actor": "han",
+          "permission": "active"
+        }
+      ],
+      "data": {
+        "from": "han",
+        "to": "eosio",
+        "quantity": "0.0001 SYS",
+        "memo": "m"
+      }
+    }
+  ],
+  "transaction_extensions": [],
+  "context_free_data": []
+}
+```
+
+:::caution
+
+:::
+Nothing under here to add
+
+# No Looping 
