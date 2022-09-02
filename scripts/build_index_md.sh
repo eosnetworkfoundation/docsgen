92d7744a7d48211804452143be5404e5deba2e4c
Eric Passmore <eric.passmore@gmail.com>
2022-09-02 15:49:59 -0700

    Fixed eosjs index issues
    - fixed '#' symbol at start of README.md
    - removed duplicate README typedoc copies README.md which is not what we want
    - create index_build.sh to build indexes when needed
    - replaced typedoc generated index file with our improved versions

---
+++ scripts/build_index_md.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+Help() {
+  echo "builds an index.md for a directory results to stdout "
+  echo "required arguments"
+  echo " -d directory to index"
+  echo " -n name of index"
+  echo "optional args"
+  echo " [-f include files]"
+  echo " [-x include directories]"
+  echo "example: build_index_md.sh -d classes -n 'API Class List' -f -x > classes/index.md"
+  exit 1
+}
+
+# Get the options
+while getopts "d:n:fx" option; do
+   case $option in
+      d) # set build dir
+        ARG_DIR=${OPTARG}
+        ;;
+      n) # set branch
+        ARG_NAME=${OPTARG}
+        ;;
+      f) # set tag
+        ARG_FILE_TYPE=1
+        ;;
+      x) # set identity
+        ARG_DIR_TYPE=1
+        ;;
+      :) # no args
+        Help;
+        ;;
+      *) # abnormal args
+        Help;
+        ;;
+   esac
+done
+# check for required args
+if [ -z "${ARG_DIR}" ] || [ -z "${ARG_NAME}" ]; then
+  echo "Missing required arguments -d directory -n name"
+  Help;
+fi
+
+# default both files and directories
+# default handles case of both and neither
+type_arg=(\( -type f -o -type d \))
+# just dirs
+if [ -z "${ARG_FILE_TYPE}" ] && [ -n "$ARG_DIR_TYPE" ]; then
+  type_arg=(-type d)
+fi
+# just files
+if [ -n "${ARG_FILE_TYPE}" ] && [ -z "$ARG_DIR_TYPE" ]; then
+  type_arg=(-type f)
+fi
+
+
+# max depth just 1 level, min depth skip directory name
+#CMD="find \"${ARG_DIR}\" ${type_arg} -maxdepth 1 -mindepth 1 -print0 "
+find_args=("${ARG_DIR}" "${type_arg[@]}" -maxdepth 1 -mindepth 1 -print0)
+
+echo "---"
+echo "title: ${ARG_NAME}"
+echo "sidebar_position: 1"
+echo "---"
+echo ""
+echo "# ${ARG_NAME}"
+echo ""
+set -x
+find "${find_args[@]}" | while IFS= read -r -d '' file
+do
+  base_name=$(basename "$file")
+  # skip . files and main dir
+  if [ -d "$file" ]; then
+    display_name=$base_name
+    base_name="${base_name}/"
+  else
+    display_name=$(echo "$base_name" | cut -d. -f1)
+    # base_name unchanged
+  fi
+  [ ! "${base_name:0:1}" = '.' ] && echo "- [${display_name}](${base_name})"
+done
--- scripts/install_mandel-eosjs.sh
+++ scripts/install_mandel-eosjs.sh
@@ -2,9 +2,12 @@
 
 Install_Mandel-eosjs() {
   SCRIPT_DIR=$1
+  # shellcheck disable=SC2034  # Unused variables to keep method call enforced
   ARG_GIT_REPO=$2
   ARG_BUILD_DIR=$3
+  # shellcheck disable=SC2034  # Unused variables to keep method call enforced
   ARG_BRANCH=$4
+  # shellcheck disable=SC2034  # Unused variables to keep method call enforced
   ARG_TAG=$5
 
   # required dependancy
@@ -12,14 +15,35 @@ Install_Mandel-eosjs() {
   # generate the docs there is a typedoc.json config
   node_modules/typedoc/bin/typedoc --plugin typedoc-plugin-markdown
 
+  # We need to move things around
+  # typedoc-out will become SDK API
+  # and SDK API will be nested under typedoc-out
+  # replace bad index (modules.md) with good index (index.md)
+  [ -d "SDK Reference" ] && rm -rf "SDK Reference"
+  mv typedoc-out "SDK Reference"
+  rm "SDK Reference"/modules.md
+  # typedoc copies README into place and we don't want that
+  rm "SDK Reference"/README.md
+
+  "${SCRIPT_DIR}"/build_index_md.sh -d "SDK Reference" -n 'eosjs Code References' > "SDK Reference"/index.md
+  find "SDK Reference" -type f -name "*.md" -print0 | while IFS= read -r -d '' file
+  do
+    sed 's/(..\/modules.md)/(..\/index.md)/g' "$file" > tempIDX.md
+    mv tempIDX.md "$file"
+  done
+
+  [ -d markdown_out ] && rm -rf markdown_out
+  mkdir markdown_out
+  mv "SDK Reference" markdown_out
+
+
   # add contributing and license files
-  mv ./CONTRIBUTING.md typedoc-out/
+  cp ./CONTRIBUTING.md markdown_out/
   # remove reference to LICENSE
-  grep -v LICENSE typedoc-out/README.md > tmp_README.md
-  mv tmp_README.md typedoc-out/README.md
+  grep -v LICENSE README.md > markdown_out/README.md
 
   # copy files in, view framework will convert from Markdown to HTML
-  [ -d ${ARG_BUILD_DIR}/devdocs/eosdocs/eosjs ] && rm -rf ${ARG_BUILD_DIR}/devdocs/eosdocs/eosjs
-  mkdir -p ${ARG_BUILD_DIR}/devdocs/eosdocs/eosjs
-  cp -R typedoc-out/* ${ARG_BUILD_DIR}/devdocs/eosdocs/eosjs
+  [ -d "${ARG_BUILD_DIR}"/devdocs/eosdocs/eosjs ] && rm -rf "${ARG_BUILD_DIR:?}"/devdocs/eosdocs/eosjs
+  mkdir -p "${ARG_BUILD_DIR:?}"/devdocs/eosdocs/eosjs || exit 1
+  cp -R markdown_out/* "${ARG_BUILD_DIR:?}"/devdocs/eosdocs/eosjs
 }
