6e442fc4319b11d975a44e17699101878c22edac
Eric Passmore <eric.passmore@gmail.com>
2022-09-07 22:32:13 -0700

    scrape 5 pages

--- crawler/pageScraper.js
+++ crawler/pageScraper.js
@@ -7,21 +7,31 @@ let property_file_name = arguments.parse();
 console.log(`opening prop file ${property_file_name}`)
 // get the properties
 const properties = propertiesReader(property_file_name);
-// set properties 
-const url = properties.get('main.url');
+// set properties
+var url = properties.get('main.url');
 const user = properties.get('main.app.user');
 const password = properties.get('main.app.password');
 
 const scraperObject = {
-
-
 	async scraper(browser){
-		let page = await browser.newPage();
-		console.log(`Navigating to ${url}...`);
-    // set the HTTP Basic Authentication credential
-    await page.authenticate({'username': user, 'password': password });
-		await page.goto(url);
-
+    // loop 5 times
+    for (let i = 0; i < 5; i++) {
+        let page = await browser.newPage();
+        console.log(`Navigating to ${url}...`);
+        // set the HTTP Basic Authentication credential
+        await page.authenticate({'username': user, 'password': password });
+        await page.goto(url);
+        // Wait for the required DOM to be rendered
+		    await page.waitForSelector('a.pagination-nav__link--next');
+		    // Get the link to all the required books
+		    url = await page.$eval('a.pagination-nav__link--next', link => {
+			      // Extract the links from the data
+			      link = link.href;
+			      return link;
+		    });
+        let title = await page.$eval('header > h1', text => text.textContent);
+        console.log(title)
+    }
 	}
 }
 
