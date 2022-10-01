1991fac30ef610e25662a394885395fbdddc7c02
Eric Passmore <eric.passmore@gmail.com>
2022-09-30 17:13:24 -0700

    tell shellcheck ignore source files

--- scripts/install_mandel.cdt.sh
+++ scripts/install_mandel.cdt.sh
@@ -8,6 +8,7 @@ ARG_BUILD_DIR=$3
 ARG_BRANCH=$4
 ARG_TAG=$5
 
+# shellcheck source=/dev/null
 source "${SCRIPT_DIR:?}"/install_cdt.sh || exit
 Install_Cdt "$SCRIPT_DIR" "${ARG_GIT_REPO:?}" "$ARG_BUILD_DIR" "$ARG_BRANCH" "$ARG_TAG" || exit
 }
--- scripts/install_mandel.sh
+++ scripts/install_mandel.sh
@@ -7,6 +7,8 @@ ARG_GIT_REPO="AntelopeIO/leap"
 ARG_BUILD_DIR=$3
 ARG_BRANCH=$4
 ARG_TAG=$5
+
+# shellcheck source=/dev/null
 source "${SCRIPT_DIR:?}"/install_leap.sh || exit
 Install_Leap "$SCRIPT_DIR" "${ARG_GIT_REPO:?}" "$ARG_BUILD_DIR" "$ARG_BRANCH" "$ARG_TAG" || exit
 }
