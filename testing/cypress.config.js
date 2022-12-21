b75576ffd2d7752c8a06e3165470cdc6ff38e94e
Eric Passmore <eric.passmore@gmail.com>
2022-12-20 17:35:55 -0800

    configuralbe hosts for tests

--- testing/cypress.config.js
+++ testing/cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ENF-docsgen',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
+    }
+  },
+  env: {
+      proto: 'http',
+      host: 'localhost:3000',
+      contracts_repo: 'system-contracts'
   },
   video: false,
 });
