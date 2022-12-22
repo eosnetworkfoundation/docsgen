1027d110012c1488786c570574e4dbe25aa6bd74
Eric Passmore <eric.passmore@gmail.com>
2022-12-22 14:49:53 -0800

    fix missing ENF directory

--- scripts/linux-host/wrapper_clean_rebuild.sh
+++ scripts/linux-host/wrapper_clean_rebuild.sh
@@ -7,4 +7,4 @@ pushd /home/fedevops/"$LOC"/docsgen || exit
 BRANCH=$(git rev-parse --abbrev-ref HEAD 2>>/dev/null)
 git pull origin "$BRANCH"
 popd || exit
-/home/fedevops/"$LOC"/docsgen/scripts/clean_rebuild.sh -d /home/fedevops/ENF/build_dir/ -y -s 2>>/home/fedevops/"$LOC"/logs/error_"$NOW".log >>/home/fedevops/"$LOC"/logs/nightly_staging_build_"$NOW".log
+/home/fedevops/"$LOC"/docsgen/scripts/clean_rebuild.sh -d /home/fedevops/"$LOC"/build_dir/ -y -s 2>>/home/fedevops/"$LOC"/logs/error_"$NOW".log >>/home/fedevops/"$LOC"/logs/nightly_staging_build_"$NOW".log
